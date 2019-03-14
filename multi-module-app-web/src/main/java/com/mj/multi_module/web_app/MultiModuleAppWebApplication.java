ce110a6cceb746200a8f2f221e232ffe2c597b5a
leemj <leemj@sph.com.sg>
2019-03-15 00:31:11 +0800

    Initial commit

---
+++ multi-module-app-web/src/main/java/com/mj/multi_module/web_app/MultiModuleAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.mj.multi_module.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleAppWebApplication.class, args);
+	}
+
+}
